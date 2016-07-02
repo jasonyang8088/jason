0775ed27e9335989ca73d941f0ea67fb2a08800c
jason <jason@T440>
2016-07-02 20:15:02 +0800

    1

---
+++ mycode/src/main/java/com/jason/demo/Application.java
@@ -0,0 +1,13 @@
+package com.jason.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+public class Application {
+
+	public static void main(String[] args) {
+		System.setProperty("spring.devtools.restart.enabled", "false");
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ mycode/src/main/java/com/jason/demo/sercurityUser/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.jason.demo.sercurityUser.controller;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String home(Map<String, Object> model) {
+		model.put("message", "Hello World");
+		model.put("title", "Hello Home");
+		model.put("date", new Date());
+		return "home";
+	}
+
+}
